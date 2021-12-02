2f06345ede316cf303cd9157bb8b17614bff61c7
Sergio de Carvalho <scarvalhojr@gmail.com>
2021-12-02 15:16:02 +0000

    Add template for all days

---
+++ day11/src/main.rs
@@ -0,0 +1,54 @@
+use clap::{crate_description, App, Arg};
+use day11::{part1, part2};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::num::ParseIntError;
+use std::process::exit;
+
+fn main() {
+    let args = App::new(crate_description!())
+        .arg(
+            Arg::with_name("INPUT")
+                .help("File with puzzle input")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    println!(crate_description!());
+
+    let input = match read_input(args.value_of("INPUT").unwrap()) {
+        Ok(data) => data,
+        Err(err) => {
+            println!("Failed to read input: {}", err);
+            exit(2);
+        }
+    };
+
+    match part1(&input) {
+        Some(answer) => println!("Part 1: {}", &answer),
+        None => println!("Part 1: Not found"),
+    }
+    match part2(&input) {
+        Some(answer) => println!("Part 2: {}", &answer),
+        None => println!("Part 2: Not found"),
+    }
+}
+
+fn read_input(filename: &str) -> Result<Vec<i32>, String> {
+    let input_file = File::open(filename).map_err(|err| err.to_string())?;
+
+    BufReader::new(input_file)
+        .lines()
+        .zip(1..)
+        .map(|(line, line_num)| {
+            line.map_err(|err| (line_num, err.to_string()))
+                .and_then(|value| {
+                    value.parse().map_err(|err: ParseIntError| {
+                        (line_num, err.to_string())
+                    })
+                })
+        })
+        .collect::<Result<_, _>>()
+        .map_err(|(line_num, err)| format!("Line {}: {}", line_num, err))
+}
---
+++ day21/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn part1(_input: &[i32]) -> Option<i32> {
+    None
+}
+
+pub fn part2(_input: &[i32]) -> Option<i32> {
+    None
+}
