e5305736c9cca398fc689043b7e055cfc4cdf6c7
Sergio de Carvalho <scarvalhojr@gmail.com>
2021-12-07 16:35:49 +0000

    Day 7

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,7 +1,31 @@
-pub fn part1(_input: &[i32]) -> Option<i32> {
-    None
+fn total_distance_simple(positions: &[i32], target: i32) -> i32 {
+    positions.iter().map(|&pos| (pos - target).abs()).sum()
 }
 
-pub fn part2(_input: &[i32]) -> Option<i32> {
-    None
+fn total_distance_incremental(positions: &[i32], target: i32) -> i32 {
+    positions
+        .iter()
+        .map(|&pos| {
+            let dist = (pos - target).abs();
+            (dist * dist + dist) / 2
+        })
+        .sum()
+}
+
+pub fn part1(positions: &[i32]) -> Option<i32> {
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+
+    (min..=max)
+        .map(|num| total_distance_simple(positions, num))
+        .min()
+}
+
+pub fn part2(positions: &[i32]) -> Option<i32> {
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+
+    (min..=max)
+        .map(|num| total_distance_incremental(positions, num))
+        .min()
 }
--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,8 +1,6 @@
 use clap::{crate_description, App, Arg};
 use day07::{part1, part2};
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::num::ParseIntError;
+use std::fs::read_to_string;
 use std::process::exit;
 
 fn main() {
@@ -36,19 +34,13 @@ fn main() {
 }
 
 fn read_input(filename: &str) -> Result<Vec<i32>, String> {
-    let input_file = File::open(filename).map_err(|err| err.to_string())?;
-
-    BufReader::new(input_file)
-        .lines()
-        .zip(1..)
-        .map(|(line, line_num)| {
-            line.map_err(|err| (line_num, err.to_string()))
-                .and_then(|value| {
-                    value.parse().map_err(|err: ParseIntError| {
-                        (line_num, err.to_string())
-                    })
-                })
+    read_to_string(filename)
+        .map_err(|err| err.to_string())?
+        .split(',')
+        .map(|num| {
+            num.trim()
+                .parse()
+                .map_err(|err| format!("Invalid position '{}': {}", num, err))
         })
         .collect::<Result<_, _>>()
-        .map_err(|(line_num, err)| format!("Line {}: {}", line_num, err))
 }
